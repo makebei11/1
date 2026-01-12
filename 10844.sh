60228b913fbdb6bd0161983d9cc7c1b285b7676d
makebei11 <makebei11@outlook.com>
2026-01-12 11:23:38 +0800

    Update 10844.sh

--- 10844.sh
+++ 10844.sh
@@ -2,7 +2,7 @@
 
 # =================配置区域=================
 # 设置监听端口
-LISTEN_PORT=30202
+LISTEN_PORT=10585
 # 设置固定密码
 PASSWORD='AAAACchacha20chacha209AAAAA'
 # 设置加密方式 (推荐 aes-256-gcm或chacha20-ietf-poly1305)
